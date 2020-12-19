d369ab480d57e77db35e79fa54b8c0f990cfce30
Laura Isotalo <l.isotalo@student.maastrichtuniversity.nl>
2020-12-19 20:30:26 +0100

    Load client parameters outside client method

--- client.py
+++ client.py
@@ -7,7 +7,35 @@ import websockets
 import json
 import re
 
+import base64
+
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
 from Connection import Connection
+
+if len(sys.argv) > 1:
+    config_file_path = sys.argv[1]
+else:
+    config_file_path = input("Enter the path to your configuration file: ")
+with open(config_file_path) as json_file:
+    client_data = json.load(json_file)
+
+# Load client parameters
+id = client_data['person']['id']
+last_name = client_data['person']['name'].split(',')[0]
+first_name = client_data['person']['name'].split(',')[1]
+public_key = client_data['person']['keys']['public']
+private_key = client_data['person']['keys']['private']
+duration = int(client_data['general']['duration'])
+retries = int(client_data['general']['retries'])
+timeout = int(client_data['general']['timeout'])
+server_ip = client_data['server']['ip']
+server_port = client_data['server']['port']
+actions = client_data['actions']
+
+# Create the list of information of actions, in the form of [[recipient0, message0], [recipient1, message1], ..]
+actions_info = [re.findall(r'\[(.*?)\]', action) for action in actions]
 
 
 async def client():
@@ -15,29 +43,6 @@ async def client():
     async with websockets.connect(uri) as websocket:
 
         connection = Connection(websocket)
-
-        if len(sys.argv) > 1:
-            config_file_path = sys.argv[1]
-        else:
-            config_file_path = input("Enter the path to your configuration file: ")
-        with open(config_file_path) as json_file:
-            client_data = json.load(json_file)
-
-        # Load client parameters
-        id = client_data['person']['id']
-        last_name = client_data['person']['name'].split(',')[0]
-        first_name = client_data['person']['name'].split(',')[1]
-        public_key = client_data['person']['keys']['public']
-        private_key = client_data['person']['keys']['private']
-        duration = int(client_data['general']['duration'])
-        retries = int(client_data['general']['retries'])
-        timeout = int(client_data['general']['timeout'])
-        server_ip = client_data['server']['ip']
-        server_port = client_data['server']['port']
-        actions = client_data['actions']
-
-        # Create the list of information of actions, in the form of [[recipient0, message0], [recipient1, message1], ..]
-        actions_info = [re.findall(r'\[(.*?)\]', action) for action in actions]
 
         await asyncio.gather(
             register(connection, id, first_name, last_name, public_key),  # Register at the server
@@ -59,7 +64,7 @@ async def register(connection: Connection, id: str, first_name: str, last_name: 
 async def receive_messages(connection: Connection):
     async for message in connection.receive_many():
         try:
-            print(message)
+            print(message['data'])
             await connection.report_success(message['id'])
         except:
             print("Failed to receive message...")
